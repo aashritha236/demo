da13c90f77e0bae451313b5d742154f255936b8e
Aashritha <aashrithamittapally@gmail.com>
2025-12-19 17:45:26 +0530

    new file added

--- hello.java
+++ hello.java
@@ -1,6 +1,5 @@
-class demo{
-    public static void main(String[] args){
-        System.out.println("Hello!");
-        System.out.println("Hello!");
-    }
+class hello{
+        public static void main(String[] args){
+            System.out.println("Git Workshop.");
+        }
 }
